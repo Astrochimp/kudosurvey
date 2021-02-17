3fc53217927f24e7c4725e299e4e5fd2a40043b8
Marcello Prattico <chimp@astrochimp.com>
2021-02-17 18:52:47 -0500

    updated test

--- components/tests/Header.test.js
+++ components/tests/Header.test.js
@@ -15,4 +15,23 @@ describe("header", () => {
     const titleText = getByText(/Kudo survey/i);
     expect(titleText).toBeInTheDocument();
   });
+
+  test("renders user menu", async () => {
+    useAuth.mockReturnValue({
+      user: {
+        id: "julian",
+        name: "Julian Manandhar",
+        avatarURL: "",
+        answers: {
+          vthrdm985a262al8qx3do: "optionOne",
+          xj352vofupe1dqz9emx13r: "optionTwo",
+        },
+        questions: ["loxhs1bqm25b708cmbf3g", "vthrdm985a262al8qx3do"],
+      },
+    });
+
+    const { getByText } = render(<Header />);
+    const titleText = getByText(/Leaderboard/i);
+    expect(titleText).toBeInTheDocument();
+  });
 });
